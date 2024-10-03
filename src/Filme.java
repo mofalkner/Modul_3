7dc89a19d7712effa382719e26fc7a6c4bb8b3a7
mfalkner <mofalknerhakimst.rocks>
2024-10-03 13:45:42 +0200

    Die Klassen "eBooks" und "Filme" wurden erstellt und alle Instanzvariablen wurden hinzugefügt.

---
+++ src/Filme.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Filme
+{
+    private String Titel;
+    private String Regisseur;
+    private double Spielzeit;
+    private boolean UHD;
+    private boolean HD;
+    private String Kommentar;
+    private Date Erscheinungsdatum;
+    private String Genre;
+}
---
+++ src/eBooks.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class eBooks
+{
+    private String Titel;
+    private String Autor;
+    private String ISBN;
+    private int Seitenanzahl;
+    private int Auflage;
+    private String Kommentar;
+    private Date Erscheinungsdatum;
+    private String Genre;
+
+
+}
