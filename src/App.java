82e6ee79a2ee61d0334a5167ccbc3f5c79c9c9d4
mfalkner <mofalknerhakimst.rocks>
2024-10-08 11:24:07 +0200

    Jetzt wurde das Modul 3 fertig gemacht, das heißt eine Medienbibliothek hinzugefügt und alles dann in der App Klasse ausprobiert!

--- src/App.java
+++ src/App.java
@@ -5,6 +5,11 @@ public class App
     public static void main(String[] args)
     {
         Filme filme = new Filme("Super Mario","Ein Film über Super Mario", LocalDate.of(2023,7,12),new Genre("Abenteuer"),new Urheber("Alessandro","Riml","Hallo GmbH"),"Moritz Falkner",90,true,false);
-        filme.anzeigen();
+        eBooks eBook = new eBooks("Paluten Freedom","In diesem Buch geht es um den Freedoms Squad!",LocalDate.of(2020,3,15),new Genre("Abenteuer"),new Urheber("Paluten","Youtuber","Freedom Firma"),"Paluten","1232-5664",160,23);
+        Medienbibliothek medienbibliothek = new Medienbibliothek();
+        medienbibliothek.mediumHinzufügen(filme);
+        medienbibliothek.mediumHinzufügen(eBook);
+        medienbibliothek.durchlaufen();
+        medienbibliothek.titelanzeigen("Paluten Freedom");
     }
 }
--- src/Medienbibliothek.java
+++ src/Medienbibliothek.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Medienbibliothek
 {
+    private ArrayList<Medium> medien = new ArrayList();
+
+    public void mediumHinzufügen(Medium medium)
+    {
+        this.medien.add(medium);
+    }
+
+    public void durchlaufen()
+    {
+        for (Medium medium : medien)
+        {
+            medium.anzeigen();
+        }
+    }
+
+    public void titelanzeigen(String titel)
+    {
+        for (Medium medium : medien)
+        {
+            if (medium.getTitel().equals(titel))
+            {
+                medium.anzeigen();
+            }
+        }
+    }
 }
